704b39502fb71b929d3f03b86951508fd7ef0e36
ammar yasser <ammarasc74@gmail.com>
2021-07-26 17:56:17 +0200

    Build: Set login & signup routes in cover section.

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import LoginForm from '../login/LoginForm';
 import './Cover.css';
-// import SignUpForm from '../signup/SignUpForm';
+import SignUpForm from '../signup/SignUpForm';
 import Button from '@material-ui/core/Button';
 
 
@@ -10,49 +10,49 @@ function Cover() {
     const history = useHistory();
     const loginOnClick = useCallback(() => history.push('/login'), [history]);
     const signUpOnClick = useCallback(() => history.push('/signup'), [history]);
+
+    useEffect(() => {
+        console.log(history.location.pathname)
+    })
     return (
-        <div className="cover">
+        <div className="cover" >
             <div className="header">
-                <div>
-                    <div className="title">
-                        <h1>Manage all your offers and items from one place.</h1>
-                    </div>
-                    <div className="button">
-                        <Button className="button1" variant="outlined" color="black" onClick={loginOnClick}>Login</Button>
-                        <Button variant="outlined" onClick={signUpOnClick}>Signup</Button>
-                    </div>
-                    <div>
-                        <div>
-                            <h1>
-                                Hello Merchant!
-                            </h1>
-                        </div>
-                        <div className="button">
-                            <LoginForm />
-                        </div>
-                        <div className="button">
-                            <Button variant="outlined">Login</Button>
-                        </div>
-                    </div>
-                    <div>
-                        {/* <div>
-                            <h1>
-                                Welcome Merchant
-                            </h1>
-                            <div className="button">
-                                <SignUpForm />
-                            </div>
-                            <div className="button">
-                                <Button variant="outlined" >Signup</Button>
-                            </div>
-                        </div> */}
-                    </div>
+                <div className="wrapper">
+                    {
+                        history.location.pathname === "/login" ?
+                            <>
+                                <div className="button">
+                                    <LoginForm />
+                                </div>
+                            </>
+                            : history.location.pathname === "/signup" ?
+                                <>
+                                    <div className="button">
+                                        <SignUpForm />
+                                    </div>
+                                </> :
+                                <>
+                                    <div className="title">
+                                        <h1>Manage all your offers and items from one place.</h1>
+                                    </div>
+                                    <div className="button">
+                                        <Button
+                                            className="button1"
+                                            variant="outlined"
+                                            color="black"
+                                            onClick={loginOnClick}>Login</Button>
+                                        <Button
+                                            variant="outlined"
+                                            onClick={signUpOnClick}>Signup</Button>
+                                    </div>
+                                </>
+                    }
                 </div>
                 <div className="header-image">
-                    <img src="https://j.top4top.io/p_2032yq0ut1.jpg" alt="" width="1500" height="700" />
+                    <img src="https://j.top4top.io/p_2032yq0ut1.jpg" alt="" width="1000" height="700" />
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
