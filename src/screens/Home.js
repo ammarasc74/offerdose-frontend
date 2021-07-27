efc6c91b922362b70abad80afb1faaa5c8567dc6
ammar yasser <ammarasc74@gmail.com>
2021-07-27 21:52:04 +0200

    Build: handling homepage and styles.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Card from "../components/cards/Card";
 import Footer from "../components/footer/Footer";
@@ -6,57 +6,39 @@ import Cover from "../components/cover/Cover";
 
 import Dashboard from "../components/cards/Dashboard.json";
 import Partner from "../components/cards/Partner.json";
-import './Home.css';
-import 'tachyons';
-
-
+import "./Home.css";
+import "tachyons";
 
 export default function Home() {
-    return (
-        <>
-            <div className="home">
-                <div className="navbar">
-                    <Navbar />
-                </div>
-                <div>
-                    <Cover />
-                </div>
-                <div className="tc">
-                    <div className="dashboard-title">
-                        <h1>Dashboard Features</h1>
-                    </div>
-                </div>
-                <div className="dashboard-section">
-                    {
-                        Dashboard.map((item, index) => {
-                            return (
-                                <Card
-                                    title={item.title}
-                                    description={item.description}
-                                />
-                            )
-                        })
-                    }
-                </div>
-                <div className="tc">
-                    <div className="dashboard-title">
-                        <h1>Partners</h1>
-                    </div>
-                </div>
-                <div className="partners-section">
-                    {
-                        Partner.map((item, index) => {
-                            return (
-                                <Card
-                                    title={item.title}
-                                    description={item.description}
-                                />
-                            )
-                        })
-                    }
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <div className="home">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="cover">
+          <Cover />
+        </div>
+        <div className="dashboard-title">
+          <h1>Dashboard Features</h1>
+        </div>
+        <div className="dashboard-section">
+          {Dashboard.map((item, index) => {
+            return <Card title={item.title} description={item.description} />;
+          })}
+        </div>
+        <div>
+          <div className="partners-title">
+            <h1>Partners</h1>
+          </div>
+        </div>
+        <div className="partners-section">
+          {Partner.map((item, index) => {
+            return <Card title={item.title} description={item.description} />;
+          })}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
