3a99e86d502d798e8ee6760e8b48c05cfa683735
ammar yasser <ammarasc74@gmail.com>
2021-07-27 21:52:24 +0200

    Build: handling signup and styles.

--- src/components/signup/SignUpForm.js
+++ src/components/signup/SignUpForm.js
@@ -1,73 +1,108 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
-import './Signup.css';
-
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import axios from "axios";
+import "./Signup.css";
 
 function SignUpForm() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const signUpRequest = () => {
-        const body = { name, email, password, confirmPassword }
-        axios.post("https://offerdose-accounts-api.herokuapp.com/user/signup", body)
-            .then((res) => console.log("res", res?.data))
-            .catch((err) => console.log("err", err))
-    }
-    return (
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const signUpRequest = () => {
+    const body = { name, email, password, confirmPassword };
+    setLoading(true);
+    axios
+      .post("https://offerdose-api-accounts.herokuapp.com/user/signup", body)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        alert("signup done, Log In now!");
+      })
+      .catch((err) => {
+        setLoading(false);
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+        alert("Sign up failed, please enter a valide data");
+      });
+  };
+  return (
+    <div>
+      <h1>Join Now!</h1>
+      <form className="form-container">
+        <TextField
+          style={{ padding: "10px", width: "400px" }}
+          value={name}
+          required
+          id="standard-required"
+          label="Name"
+          variant="outlined"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          style={{ padding: "10px", width: "400px" }}
+          value={email}
+          required
+          id="standard-required"
+          label="Email"
+          variant="outlined"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          style={{ padding: "10px", width: "400px" }}
+          value={password}
+          type="password"
+          required
+          id="standard-required"
+          label="Password"
+          variant="outlined"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <TextField
+          style={{ padding: "10px", width: "400px" }}
+          value={confirmPassword}
+          type="password"
+          required
+          id="standard-required"
+          label="Confirm Password"
+          variant="outlined"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <div>
-            <div>
-                <h1>
-                    Join Now!
-                </h1>
-            </div>
-            <form>
-                <TextField
-                    required id="standard-required"
-                    label="Name"
-                    variant="outlined"
-                    onChange={(e) =>
-                        setName(e.target.value)}
-                />
-                <TextField
-                    required id="standard-required"
-                    label="Email"
-                    variant="outlined"
-                    onChange={(e) =>
-                        setEmail(e.target.value)} />
-                <TextField
-                    type="password"
-                    required id="standard-required"
-                    label="Password"
-                    variant="outlined"
-                    onChange={(e) =>
-                        setPassword(e.target.value)
-                    }
-                />
-                <TextField
-                    type="password"
-                    required id="standard-required"
-                    label="Confirm Password"
-                    variant="outlined"
-                    onChange={(e) =>
-                        setConfirmPassword(e.target.value)} />
-            </form>
-            <div className="button">
-                <Button
-                    variant="outlined"
-                    onClick={() => signUpRequest()}
-                >
-                    Signup
-                </Button>
-            </div>
-            <h4>Do you have an account ? <Link to="/login">Login Now</Link> </h4>
+          {loading === true ? (
+            <CircularProgress
+              style={{
+                color: "primary",
+                paddingRight: "10px",
+                alignItems: "center",
+              }}
+            />
+          ) : (
+            <Button
+              style={{
+                backgroundColor: "black",
+                color: "white",
+              }}
+              variant="outlined"
+              onClick={() => signUpRequest()}
+            >
+              Signup
+            </Button>
+          )}
         </div>
-    )
-
+        <h4 className="forget-text">
+          Do you have an account? <Link to="/login">Log In</Link>{" "}
+        </h4>
+      </form>
+    </div>
+  );
 }
 export default SignUpForm;
