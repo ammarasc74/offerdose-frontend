f85bcd2e87fb44a2a452f607a34e57e5e80937fc
ammar yasser <ammarasc74@gmail.com>
2021-07-27 21:53:24 +0200

    Build: handling navbar and styles.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-// import Button from '@material-ui/core/Button';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <>
-            <nav className="navbar">
-                <div className="nav-logo">
-                    <div>
-                        <img src="https://j.top4top.io/p_2032yq0ut1.jpg" alt="" width="30" height="30" />
-                        <Link to="/" className="navbar-logo">
-                            OfferDose
-                        </Link>
-                    </div>
-                    <div className="navbar-menu">
-                        <Link to="/" className="nav">Dashboard</Link>
-                        <Link to="/" className="nav">Features</Link>
-                        <Link to="/" className="nav">Support</Link>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-logo">
+          <Link to="/" className="navbar-logo">
+            <div className="img-logo">
+              <Avatar
+                alt="Remy Sharp"
+                src="https://e.top4top.io/p_203499sx11.jpg"
+              />
+            </div>
+            OfferDose
+          </Link>
+          <div className="navbar-menu">
+            <Link to="/" className="nav">
+              Dashboard
+            </Link>
+            <Link to="/" className="nav">
+              Features
+            </Link>
+            <Link to="/" className="nav">
+              Support
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
